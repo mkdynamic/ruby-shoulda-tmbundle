57b85ad27c4187b0b29004425bce4ad19d1472fe
James Adam <james@lazyatom.com>
2009-01-16 10:56:20 +0000

    Fixed parsing of test names to include tests outside of contexts (at least when they are at the top of the script).

--- Support/print_shoulda_test_name.rb
+++ Support/print_shoulda_test_name.rb
@@ -4,17 +4,22 @@ end
 
 n = ENV['TM_LINE_NUMBER'].to_i
 
-should, context = nil, nil
+should, context, class_name = nil, nil, nil
 
 File.open(ENV['TM_FILEPATH']) do |f|
   lines     = f.read.split("\n")[0...n].reverse
   context   = lines.find { |line| line =~ /^\s*context\b/ }
   should    = parse_from_quotes(lines.find { |line| line =~ /^\s*should\b/ })
+  lines.find { |line| line =~ /^\s*class (\w*)Test/ }
+  class_name = $1
+  puts class_name
 end
 
-if !context.empty? &amp;&amp; !should.empty?
+if (context || class_name) && should
   puts "shoulda"
   context = parse_from_quotes( context )
   name = "#{context} should #{should}".gsub(/[\+\.\s\'\"\(\)]/,'.')
   print "--name=/#{name}/ -v v"  
+else
+  raise "No test could be automatically identified."
 end
